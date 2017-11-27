93bc910c5b506b65192ba3305594438cbf1411a9
Daniel Grabowski <dmgrabowski@gmail.com>
2017-11-26 23:10:02 -0500

    Added NVIDIA model

--- model.py
+++ model.py
@@ -1,21 +1,44 @@
 import csv
 import cv2
-from keras.layers import Input, Flatten, Dense
+from keras.layers import Input, Flatten, Dense, Lambda, Conv2D, Dropout
 from keras.models import Sequential
 import numpy as np
 
+
+def build_model():
+    model = Sequential()
+    model.add(Lambda(lambda x: x/127.5-1.0, input_shape=(160,320,3)))
+    model.add(Conv2D(24, 5, 5, activation='elu', subsample=(2, 2)))
+    model.add(Conv2D(36, 5, 5, activation='elu', subsample=(2, 2)))
+    model.add(Conv2D(48, 5, 5, activation='elu', subsample=(2, 2)))
+    model.add(Conv2D(64, 3, 3, activation='elu'))
+    model.add(Conv2D(64, 3, 3, activation='elu'))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(100, activation='elu'))
+    model.add(Dense(50, activation='elu'))
+    model.add(Dense(10, activation='elu'))
+    model.add(Dense(1))
+    model.summary()
+
+    return model
+
+
 lines = []
-with open('data/driving_log.csv') as csvfile:
+with open('./data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
+        
+        
+lines.pop(0)
 
 images = []
 measurements = []
 for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
-    current_path = "data/IMG"+filename
+    current_path = "./data/IMG/"+filename
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
@@ -24,9 +47,7 @@ for line in lines:
 X_train = np.array(images)
 y_train = np.array(measurements)
 
-model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
-model.add(Dense(1))
+model = build_model()
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
