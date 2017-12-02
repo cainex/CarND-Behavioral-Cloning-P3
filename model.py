a4fe509667483614c4d520427c07492132444bda
Daniel Grabowski <dmgrabowski@gmail.com>
2017-12-02 18:29:14 -0500

    Added argument parsing to model.py, updated gitignore to ignore test_run data, added test_run.mp4 video

--- model.py
+++ model.py
@@ -1,12 +1,23 @@
 import csv
 import cv2
+
+from keras import backend as K
+
+if 'tensorflow' == K.backend():
+    print('setting Tensorflow session configuration')
+    import tensorflow as tf
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    K.set_session(tf.Session(config=config))
+
 from keras.layers import Input, Flatten, Dense, Lambda, Conv2D, Dropout, Cropping2D
 from keras.models import Sequential, load_model
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 import matplotlib.image as mpimg
+import argparse
 
 def load_data(path):
     lines = []
@@ -19,7 +30,7 @@ def load_data(path):
 
     return lines
 
-def generator(path, samples, batch_size=32):
+def generator(samples, batch_size=32):
     num_samples = len(samples)
     correction = 0.2
 
@@ -31,36 +42,9 @@ def generator(path, samples, batch_size=32):
             images = []
             angles = []
             for batch_sample in batch_samples:
-                # name = '{}/IMG/'.format(path)+batch_sample[0].split('/')[-1]
-                # left_name = '{}/IMG/'.format(path)+batch_sample[1].split('/')[-1]
-                # right_name = '{}/IMG/'.format(path)+batch_sample[2].split('/')[-1]
                 name = batch_sample[0]
                 left_name = batch_sample[1]
                 right_name = batch_sample[2]
-
-                # center_image = cv2.cvtColor(cv2.imread(name), cv2.COLOR_RGB2YUV)
-#                 choice = np.random.choice(3)
-
-#                 if choice == 0:
-#                     center_image = mpimg.imread(name)
-#                     # center_image = cv2.imread(name)
-#                     center_angle = float(batch_sample[3])
-#                     images.append(center_image)
-#                     angles.append(center_angle)
-#                 elif choice == 1:
-# #                    left_image = cv2.cvtColor(cv2.imread(left_name), cv2.COLOR_RGB2YUV)
-# #                    left_image = cv2.imread(left_name)
-#                     left_image = mpimg.imread(left_name)
-#                     left_angle = float(batch_sample[3]) + correction
-#                     images.append(left_image)
-#                     angles.append(left_angle)
-#                 else:
-#  #                   right_image = cv2.cvtColor(cv2.imread(left_name), cv2.COLOR_RGB2YUV)
-# #                    right_image = cv2.imread(right_name)
-#                     right_image = mpimg.imread(right_name)
-#                     right_angle = float(batch_sample[3]) - correction
-#                     images.append(right_image)
-#                     angles.append(right_angle)
 
                 center_image = mpimg.imread(name)
                 center_angle = float(batch_sample[3])
@@ -94,12 +78,11 @@ def build_model():
     This will build the Keras model. This follows the NVIDIA self-driving car neural network
     """
     model = Sequential()
-#    model.add(Lambda(lambda x: cv2.cvtColor(np.array(x), cv2.COLOR_RGB2YUV), input_shape=(160,320,3)))
     model.add(Lambda(lambda x: x/127.5-1.0, input_shape=(160,320,3)))
     model.add(Cropping2D(cropping=((50,20), (0,0))))
-    model.add(Conv2D(24, (5, 5), activation='elu', subsample=(2, 2)))
-    model.add(Conv2D(36, (5, 5), activation='elu', subsample=(2, 2)))
-    model.add(Conv2D(48, (5, 5), activation='elu', subsample=(2, 2)))
+    model.add(Conv2D(24, (5, 5), activation='elu', strides=(2, 2)))
+    model.add(Conv2D(36, (5, 5), activation='elu', strides=(2, 2)))
+    model.add(Conv2D(48, (5, 5), activation='elu', strides=(2, 2)))
     model.add(Conv2D(64, (3, 3), activation='elu'))
     model.add(Conv2D(64, (3, 3), activation='elu'))
     model.add(Dropout(0.5))
@@ -112,32 +95,36 @@ def build_model():
 
     return model
 
-path = '/mnt/raid/projects/udacity/sdc_nd/datasets/behavioral/set_combined'
-samples = load_data(path)
+if __name__ == "__main__":
 
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+    parser = argparse.ArgumentParser(description='Self-driving car simulation training script')
+    parser.add_argument('-p', '--path', help='path to dataset CSV', dest='csv_path', type=str)
+    parser.add_argument('-b', '--batch_size', help='size of batch', dest='batch_size', type=int, default=32)
+    parser.add_argument('-n', '--num_epochs', help='number of epochs to run', dest='num_epochs', type=int, default=20)
+    parser.add_argument('-m', '--model_name', help='output filename for saved model', dest='model_name', type=str, default='model.h5')
+    args = parser.parse_args()
 
-batch_size = 32
-num_epochs = 20
-# num_steps_per_epoch = 500
-# num_validation_steps = num_steps_per_epoch * 0.2
-num_steps_per_epoch = len(train_samples)/batch_size
-num_validation_steps = len(validation_samples)/batch_size
+    data_samples = load_data(args.csv_path)
 
-train_generator = generator(path, train_samples, batch_size=batch_size)
-validation_generator = generator(path, validation_samples)
+    train_samples, validation_samples = train_test_split(data_samples, test_size=0.2)
 
-model = build_model()
-model.compile(loss='mse', optimizer='Nadam')
+#    batch_size = 32
+#    num_epochs = 20
+    # num_steps_per_epoch = 500
+    # num_validation_steps = num_steps_per_epoch * 0.2
+    num_steps_per_epoch = len(train_samples)/args.batch_size
+    num_validation_steps = len(validation_samples)/args.batch_size
 
-# model = load_model('model.h5')
+    train_generator = generator(train_samples, batch_size=args.batch_size)
+    validation_generator = generator(validation_samples)
 
-checkpoint = ModelCheckpoint('model_hard.h5', monitor='val_loss', verbose=0, save_best_only=1, mode='auto')
+    model = build_model()
+    model.compile(loss='mse', optimizer='adam')
+
+    # model = load_model('model.h5')
+
+    checkpoint = ModelCheckpoint(args.model_name, monitor='val_loss', verbose=0, save_best_only=1, mode='auto')
+    early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=2, verbose=0, mode='auto')
 
 
-#model.fit_generator(train_generator, steps_per_epoch=len(train_samples)/batch_size, validation_data=validation_generator, validation_steps=len(validation_samples)/batch_size, epochs=num_epochs, callbacks=[checkpoint], verbose=1)
-model.fit_generator(train_generator, steps_per_epoch=num_steps_per_epoch, validation_data=validation_generator, validation_steps=num_validation_steps, epochs=num_epochs, callbacks=[checkpoint], verbose=1)
-#model.fit_generator(train_generator, steps_per_epoch=len(train_samples)/batch_size, validation_data=validation_generator, validation_steps=len(validation_samples)/batch_size, epochs=num_epochs, verbose=1)
-#model.fit_generator(train_generator, steps_per_epoch=num_steps_per_epoch, validation_data=validation_generator, validation_steps=num_validation_steps, epochs=num_epochs, verbose=1)
-
-#model.save('model.h5')
+    model.fit_generator(train_generator, steps_per_epoch=num_steps_per_epoch, validation_data=validation_generator, validation_steps=num_validation_steps, epochs=args.num_epochs, callbacks=[checkpoint, early_stop], verbose=1)
